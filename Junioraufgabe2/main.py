79339080238a312b8cbb970b96603c8e41ca738a
PPTide <52575064+PPTide@users.noreply.github.com>
2021-11-21 16:10:08 +0100

    Comments for JA2

--- Junioraufgabe2/main.py
+++ Junioraufgabe2/main.py
@@ -23,6 +23,7 @@ def main() -> None:
   data = dataFile.read()                  # speichern. 
 
 
+  # Aus Datei eine 2D Array erstellen
   dates = []
   for i in range(int(data.split(" ")[0])):
     preferences = data.splitlines()[i + 1]
@@ -32,20 +33,21 @@ def main() -> None:
     dates.append(preference)
 
 
-  # Vieleicht
-  #dates = [[j - min(i) for j in i] for i in dates]
+  # Für jede Person die benötigten Änderungen speichern
+  dates = [neededChange(i) for i in dates]
 
-  neededChanges = [neededChange(i) for i in dates]
+  # Nach Termin anstatt Person sortieren. 
+  dates = list(zip(*dates))
 
+  # Die Summe der benötigten Terminänderungen speichern
+  dates = [sum(i) for i in dates]
 
-  # rotieren (+spiegeln)
-  neededChangesR = list(zip(*neededChanges))
+  # Für den Termin mit der kleinsten Menge an Terminänderungen
+  # die Anzahl an Terminänderungen und die Terminnummer spiechern
+  solutionCost = min(dates)
+  solutionDate = dates.index(solutionCost)
 
-  neededChangesS = [sum(i) for i in neededChangesR]
-
-  solutionCost = min(neededChangesS)
-  solutionDate = neededChangesS.index(solutionCost)
-
+  # Als formatierten String ausgeben
   print("Für den {solutionDate}. Termin {muss} {solutionCost} {eintrag} verändert werden."
     .format(solutionDate=solutionDate+1, solutionCost=solutionCost,
     # Gramatik :)
